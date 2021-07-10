e058cc8495ceb8af448b98b7f66b01a4165b9f7f
Jonathan-Nolan <jonathan.nolan.jn@gmail.com>
2021-07-10 21:22:32 +0100

    Added the unittest and a class test.

--- Chap1/1_1_IsUnique.py
+++ Chap1/1_1_IsUnique.py
@@ -6,4 +6,26 @@ def IsUnique(s):
         if s.count(k) > 1:
             return False
 
+    if len(x) != len(s):
+        return False
+
     return True
+
+
+
+class Test(unittest.TestCase):
+    test_cases = [
+        ("abcd", True),
+        ("s4fad", True),
+        ("", True),
+        ("23ds2", False),
+        ("hb 627jh=j ()", False),
+        ("".join([chr(val) for val in range(128)]), True),  # unique 128 chars
+        ("".join([chr(val // 2) for val in range(129)]), False),  # non-unique 129 chars
+    ]
+    test_functions = [
+        IsUnique
+    ]
+
+if __name__ == "__main__":
+    unittest.main()
